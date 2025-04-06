03ab0ce6fc389e31888a33a3e8a4474e25886c1f
Sopon Hiruntiwakul <sopon.hirun536@gmail.com>
2025-04-06 15:37:49 +0700

    split py to another folder

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,21 @@
+// const config = {
+// 	app: {
+// 		port: 5005, // พอร์ตของเซิร์ฟเวอร์
+// 	},
+// 	db: {
+// 		host: "postgres://neondb_owner:npg_hIn4tfJ5YEMz@ep-lively-heart-a1gidlp4-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require",
+// 		port: 3306, // พอร์ตของ MySQL
+// 		user: "neondb_owner", 
+// 		password: "npg_hIn4tfJ5YEMz",
+// 		database: "neondb",
+// 	},
+// }
+
+// console.log("env working",config);
+
+// export default config
+
+
 const config = {
 	app: {
 		port: 5005, // พอร์ตของเซิร์ฟเวอร์
